0a0d0640e58b7e67dcc504be2833dd65572cd1d7
Meghan Denny <hello@nektro.net>
2023-09-27 12:25:47 -0700

    shell.nix: use system packages

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
-(import
-  (
-    let flake-compat = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.flake-compat; in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${flake-compat.locked.rev}.tar.gz";
-      sha256 = flake-compat.locked.narHash;
-    }
-  )
-  { src = ./.; }).shellNix
+with import <nixpkgs> {};
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkgconfig
+    gtk4.dev
+  ];
+
+  hardeningDisable = [ "all" ];
+}
