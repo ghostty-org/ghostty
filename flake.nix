fd704ac17c2f05ed9849cb64ccd678c876dc62c9
Jeffrey C. Ollie <jeff@ocjtech.us>
2024-01-08 13:55:28 -0600

    Update to NixOS 23.11 but keep option to build with 23.05

--- flake.nix
+++ flake.nix
@@ -7,7 +7,8 @@
     # We want to stay as up to date as possible but need to be careful that the
     # glibc versions used by our dependencies from Nix are compatible with the
     # system glibc that the user is building for.
-    nixpkgs-stable.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs-23-05.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs-23-11.url = "github:nixos/nixpkgs/release-23.11";
 
     # This is a nixpkgs mirror (based off of master) that contains
     # patches for Zig 0.12 (master/nightly).
@@ -21,46 +22,58 @@
 
     zig = {
       url = "github:mitchellh/zig-overlay";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
+      inputs.nixpkgs.follows = "nixpkgs-23-11";
     };
 
     zls = {
       url = "github:zigtools/zls/master";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
+      inputs.nixpkgs.follows = "nixpkgs-23-11";
+      inputs.zig-overlay.follows = "zig";
     };
   };
 
   outputs = {
     self,
+    nixpkgs-23-05,
+    nixpkgs-23-11,
     nixpkgs-unstable,
-    nixpkgs-stable,
     nixpkgs-zig-0-12,
     zig,
     zls,
     ...
   }:
-    builtins.foldl' nixpkgs-stable.lib.recursiveUpdate {} (builtins.map (system: let
-      pkgs-stable = nixpkgs-stable.legacyPackages.${system};
+    builtins.foldl' nixpkgs-23-11.lib.recursiveUpdate {} (builtins.map (system: let
+      pkgs-23-05 = nixpkgs-23-05.legacyPackages.${system};
+      pkgs-23-11 = nixpkgs-23-11.legacyPackages.${system};
       pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
       pkgs-zig-0-12 = nixpkgs-zig-0-12.legacyPackages.${system};
     in {
-      devShell.${system} = pkgs-stable.callPackage ./nix/devShell.nix {
+      devShell.${system} = pkgs-23-11.callPackage ./nix/devShell.nix {
         inherit (pkgs-unstable) tracy;
         inherit (zls.packages.${system}) zls;
 
-        zig = zig.packages.${system}.master;
-        wraptest = pkgs-stable.callPackage ./nix/wraptest.nix {};
+        zig = zig.packages.${system}.master-2024-01-06;
+        wraptest = pkgs-23-11.callPackage ./nix/wraptest.nix {};
       };
 
       packages.${system} = rec {
-        ghostty = pkgs-stable.callPackage ./nix/package.nix {
+        ghostty-23-05 = pkgs-23-05.callPackage ./nix/package.nix {
           inherit (pkgs-zig-0-12) zig_0_12;
           revision = self.shortRev or self.dirtyShortRev or "dirty";
         };
+        ghostty-23-11 = pkgs-23-11.callPackage ./nix/package.nix {
+          inherit (pkgs-zig-0-12) zig_0_12;
+          revision = self.shortRev or self.dirtyShortRev or "dirty";
+        };
+        ghostty-unstable = pkgs-unstable.callPackage ./nix/package.nix {
+          inherit (pkgs-zig-0-12) zig_0_12;
+          revision = self.shortRev or self.dirtyShortRev or "dirty";
+        };
+        ghostty = ghostty-23-11;
         default = ghostty;
       };
 
-      formatter.${system} = pkgs-stable.alejandra;
+      formatter.${system} = pkgs-23-11.alejandra;
 
       # Our supported systems are the same supported systems as the Zig binaries.
     }) (builtins.attrNames zig.packages));
