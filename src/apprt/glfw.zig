8f8b7f1f87807223e1e65d9088f11a9dfe09f3da
Isaac Mills <rooster0055@protonmail.com>
2024-11-25 19:01:58 -0700

    Revert other work

--- src/apprt/glfw.zig
+++ src/apprt/glfw.zig
@@ -41,7 +41,6 @@ pub const App = struct {
     pub const Options = struct {};
 
     pub fn init(core_app: *CoreApp, _: Options) !App {
-        internal_os.unsetenv("DISPLAY");
         if (comptime builtin.target.isDarwin()) {
             log.warn("WARNING WARNING WARNING: GLFW ON MAC HAS BUGS.", .{});
             log.warn("You should use the AppKit-based app instead. The official download", .{});
