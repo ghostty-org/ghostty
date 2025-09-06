5f7f0d8b0aecde67aa5a6304ee36983becb6c75c
Leah Amelia Chen <hi@pluie.me>
2025-09-06 14:14:35 +0200

    build/webgen: port to Zig 0.15

--- src/build/webgen/main_actions.zig
+++ src/build/webgen/main_actions.zig
@@ -3,6 +3,15 @@ const help_strings = @import("help_strings");
 const helpgen_actions = @import("../../input/helpgen_actions.zig");
 
 pub fn main() !void {
-    const output = std.Io.getStdOut().writer();
-    try helpgen_actions.generate(output, .markdown, true, std.heap.page_allocator);
+    var buf: [4096]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&buf);
+    try helpgen_actions.generate(
+        &stdout_writer.interface,
+        .markdown,
+        true,
+        std.heap.page_allocator,
+    );
+
+    // Don't forget to flush!
+    try stdout_writer.interface.flush();
 }
