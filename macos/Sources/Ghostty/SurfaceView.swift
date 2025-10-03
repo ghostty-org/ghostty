902bfa8e04ea55359d2c43eed28b1cfe96257506
Lars <134181853+bo2themax@users.noreply.github.com>
2025-10-03 16:02:30 +0200

    SettingsUI-macOS: Apply Theme

--- macos/Sources/Ghostty/SurfaceView.swift
+++ macos/Sources/Ghostty/SurfaceView.swift
@@ -615,6 +615,8 @@ extension EnvironmentValues {
         get { self[GhosttySurfaceViewKey.self] }
         set { self[GhosttySurfaceViewKey.self] = newValue }
     }
+
+    @Entry var ghosttyConfig: Ghostty.Config?
 }
 
 extension View {
