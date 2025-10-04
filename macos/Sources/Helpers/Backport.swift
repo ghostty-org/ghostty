fdc7a481ea837939c528b3edf23e192eb5362a42
Lars <134181853+bo2themax@users.noreply.github.com>
2025-10-04 16:18:57 +0200

    SettingsUI-macOS: apply font family

--- macos/Sources/Helpers/Backport.swift
+++ macos/Sources/Helpers/Backport.swift
@@ -58,6 +58,14 @@ extension Backport where Content: View {
             return content
         }
     }
+
+    nonisolated func contentMargins(_ edges: Edge.Set = .all, _ length: CGFloat?, for placement: BackportContentMarginPlacement = .automatic) -> some View {
+        if #available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *) {
+            return content.contentMargins(edges, length, for: placement.official)
+        } else {
+            return content
+        }
+    }
 }
 
 enum BackportVisibility {
@@ -87,6 +95,20 @@ enum BackportScrollEdgeEffectStyle: Hashable, Sendable {
         }
     }
 }
+
+enum BackportContentMarginPlacement {
+    case automatic, scrollContent, scrollIndicators
+
+    @available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
+    var official: ContentMarginPlacement {
+        switch self {
+        case .automatic: return .automatic
+        case .scrollContent: return .scrollContent
+        case .scrollIndicators: return .scrollIndicators
+        }
+    }
+}
+
 enum BackportPointerStyle {
     case `default`
     case grabIdle
