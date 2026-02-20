9144b34c0bb4ea501ffabad622ae3a03d1ae64f7
Ēriks Remess <eriks@remess.lv>
2026-02-21 01:13:51 +0200

    i18n: normalize LANGUAGE environment variable and use first supported locale by order of preference

--- src/os/locale.zig
+++ src/os/locale.zig
@@ -32,41 +32,7 @@ pub fn ensureLocale(alloc: std.mem.Allocator) !void {
     if ((try internal_os.getenv(alloc, "LANGUAGE"))) |language| {
         defer language.deinit(alloc);
         if (language.value.len > 0) {
-            normalized: {
-                var locale_buf: [128]u8 = undefined;
-                var first = true;
-                // LANGUAGE might come as lv:en, without .UTF-8. Ordered by preference, we check one by one to find first valid locale.
-                var it = std.mem.splitScalar(u8, language.value, ':');
-                while (it.next()) |entry| {
-                    if (entry.len == 0) continue;
-                    defer first = false;
-
-                    // If the first preferred language is English (Ghostty's default), stop.
-                    if (first and std.mem.eql(u8, entry, "en")) {
-                        _ = internal_os.setenv("LANGUAGE", "en");
-                        break :normalized;
-                    }
-
-                    // If entry has no region (no "_*"), pick the first matching full locale.
-                    if (std.mem.indexOfScalar(u8, entry, '_') == null) {
-                        for (i18n_locales.locales) |locale| {
-                            if (!std.mem.startsWith(u8, locale, entry)) continue;
-                            if (locale.len <= entry.len) continue;
-                            if (locale[entry.len] != '_') continue;
-                            _ = internal_os.setenv("LANGUAGE", locale);
-                            break :normalized;
-                        }
-                    }
-
-                    // Normalize to {entry}.UTF-8 for LANGUAGE when supported by Ghostty.
-                    const locale = std.fmt.bufPrintZ(&locale_buf, "{s}.UTF-8", .{entry}) catch continue;
-                    for (i18n_locales.locales) |supported| {
-                        if (!std.mem.eql(u8, locale, supported)) continue;
-                        _ = internal_os.setenv("LANGUAGE", locale);
-                        break :normalized;
-                    }
-                }
-            }
+            preferredLanguage("LANGUAGE", language.value);
         }
     }
 
@@ -250,6 +216,44 @@ fn preferredLanguageFromCocoa(
     // reslice it with the null terminator.
     const slice = fbs.getWritten();
     return slice[0 .. slice.len - 1 :0];
+}
+
+fn preferredLanguage(key: [:0]const u8, language: []const u8) void {
+    var locale_buf: [128]u8 = undefined;
+    var first = true;
+
+    // LANGUAGE might come as lv:en, without .UTF-8. Ordered by preference,
+    // we check one by one to find first supported locale.
+    var it = std.mem.splitScalar(u8, language, ':');
+    while (it.next()) |entry| {
+        if (entry.len == 0) continue;
+        defer first = false;
+
+        // If the first preferred language is English (Ghostty's default), stop.
+        if (first and std.mem.eql(u8, entry, "en")) {
+            _ = internal_os.setenv(key, "en");
+            return;
+        }
+
+        // If entry has no region (no "_*"), pick the first matching full locale.
+        if (std.mem.indexOfScalar(u8, entry, '_') == null) {
+            for (i18n_locales.locales) |locale| {
+                if (!std.mem.startsWith(u8, locale, entry)) continue;
+                if (locale.len <= entry.len) continue;
+                if (locale[entry.len] != '_') continue;
+                _ = internal_os.setenv(key, locale);
+                return;
+            }
+        }
+
+        // Normalize to {entry}.UTF-8 for LANGUAGE when supported by Ghostty.
+        const locale = std.fmt.bufPrintZ(&locale_buf, "{s}.UTF-8", .{entry}) catch continue;
+        for (i18n_locales.locales) |supported| {
+            if (!std.mem.eql(u8, locale, supported)) continue;
+            _ = internal_os.setenv(key, locale);
+            return;
+        }
+    }
 }
 
 const LC_ALL: c_int = 6; // from locale.h
