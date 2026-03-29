74141a847921155c4695b5e883ba7ecc3e1a7303
Lukas <134181853+bo2themax@users.noreply.github.com>
2026-03-29 13:57:23 +0200

    macOS: add cut menu

--- macos/Sources/App/macOS/AppDelegate.swift
+++ macos/Sources/App/macOS/AppDelegate.swift
@@ -40,6 +40,7 @@ class AppDelegate: NSObject,
 
     @IBOutlet private var menuUndo: NSMenuItem?
     @IBOutlet private var menuRedo: NSMenuItem?
+    @IBOutlet private var menuCut: NSMenuItem?
     @IBOutlet private var menuCopy: NSMenuItem?
     @IBOutlet private var menuPaste: NSMenuItem?
     @IBOutlet private var menuPasteSelection: NSMenuItem?
@@ -1144,6 +1145,7 @@ extension AppDelegate {
     @MainActor private func saveRestorableMenuItems() {
         [
             menuUndo, menuRedo,
+            menuCut,
             menuCopy, menuPaste, menuSelectAll,
         ]
             .compactMap { $0 }
@@ -1185,6 +1187,7 @@ extension AppDelegate {
 
         syncMenuShortcut(config, action: "undo", menuItem: self.menuUndo)
         syncMenuShortcut(config, action: "redo", menuItem: self.menuRedo)
+        syncMenuShortcut(config, action: nil, menuItem: self.menuCut)
         syncMenuShortcut(config, action: "copy_to_clipboard", menuItem: self.menuCopy)
         syncMenuShortcut(config, action: "paste_from_clipboard", menuItem: self.menuPaste)
         syncMenuShortcut(config, action: "paste_from_selection", menuItem: self.menuPasteSelection)
@@ -1233,7 +1236,7 @@ extension AppDelegate {
         reloadDockMenu()
     }
 
-    @MainActor private func syncMenuShortcut(_ config: Ghostty.Config, action: String, menuItem: NSMenuItem?) {
+    @MainActor private func syncMenuShortcut(_ config: Ghostty.Config, action: String?, menuItem: NSMenuItem?) {
         menuShortcutManager.syncMenuShortcut(config, action: action, menuItem: menuItem)
     }
 
@@ -1346,7 +1349,8 @@ extension AppDelegate: NSMenuItemValidation {
                 item.title = "Redo"
             }
             return undoManager.canRedo
-
+        case menuCut?.action:
+            return false
         default:
             return true
         }
