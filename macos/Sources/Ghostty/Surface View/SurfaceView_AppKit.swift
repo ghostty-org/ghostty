dc8f42ed594a16d10b667227f4166d485cee78d4
Lukas <134181853+bo2themax@users.noreply.github.com>
2026-05-03 09:49:56 +0200

    macOS: restore menu shortcuts based on first responder

--- macos/Sources/Ghostty/Surface View/SurfaceView_AppKit.swift
+++ macos/Sources/Ghostty/Surface View/SurfaceView_AppKit.swift
@@ -805,9 +805,6 @@ extension Ghostty {
         override func becomeFirstResponder() -> Bool {
             let result = super.becomeFirstResponder()
             if result { focusDidChange(true) }
-            (NSApp.delegate as? AppDelegate)?
-                .restoreShortcutsRequest
-                .send()
             return result
         }
 
@@ -816,9 +813,6 @@ extension Ghostty {
             // We sometimes call this manually (see SplitView) as a way to force us to
             // yield our focus state.
             if result { focusDidChange(false) }
-            (NSApp.delegate as? AppDelegate)?
-                .restoreShortcutsRequest
-                .send()
             return result
         }
 
