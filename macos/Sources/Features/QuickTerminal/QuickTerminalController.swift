5cd3da9ce6e1450c0857101ac45f8c3a45064d04
Austin Drummond <adrum0x7@gmail.com>
2026-05-08 10:33:53 -0400

    remove unused QuickTerminalController.showNoNewTabAlert function

--- macos/Sources/Features/QuickTerminal/QuickTerminalController.swift
+++ macos/Sources/Features/QuickTerminal/QuickTerminalController.swift
@@ -654,15 +654,6 @@ class QuickTerminalController: BaseTerminalController {
         terminalViewContainer?.ghosttyConfigDidChange(ghostty.config, preferredBackgroundColor: nil)
     }
 
-    private func showNoNewTabAlert() {
-        guard let window else { return }
-        let alert = NSAlert()
-        alert.messageText = "Cannot Create New Tab"
-        alert.informativeText = "Tabs aren't supported in the Quick Terminal."
-        alert.addButton(withTitle: "OK")
-        alert.alertStyle = .warning
-        alert.beginSheetModal(for: window)
-    }
     // MARK: First Responder
 
     @IBAction override func closeWindow(_ sender: Any) {
