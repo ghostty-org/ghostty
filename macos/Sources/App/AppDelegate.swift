9066da0b63ab3519d422edffaf4d149e39efbb32
Lukas <134181853+bo2themax@users.noreply.github.com>
2026-08-05 19:26:08 +0200

    macOS: remove dead code

--- macos/Sources/App/AppDelegate.swift
+++ macos/Sources/App/AppDelegate.swift
@@ -1359,8 +1359,6 @@ extension AppDelegate: NSMenuItemValidation {
                 item.title = "Redo"
             }
             return undoManager.canRedo
-        case menuCut?.action:
-            return false
         default:
             return true
         }
