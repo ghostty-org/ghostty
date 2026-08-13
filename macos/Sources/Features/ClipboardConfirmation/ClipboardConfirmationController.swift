59913ba53aecb1a50e6221225dfaa410c2f9f183
Lukas <134181853+bo2themax@users.noreply.github.com>
2026-08-13 19:59:59 +0200

    macOS: replace ClipboardConfirmationController with RichTextAlert

--- macos/Sources/Features/ClipboardConfirmation/ClipboardConfirmationController.swift
+++ macos/Sources/Features/ClipboardConfirmation/ClipboardConfirmationController.swift
@@ -2,22 +2,24 @@ import Foundation
 import Cocoa
 import SwiftUI
 
-/// This initializes a clipboard confirmation warning window. The window itself
-/// WILL NOT show automatically and the caller must show the window via
-/// showWindow, beginSheet, etc.
-class ClipboardConfirmationController: NSWindowController {
-    override var windowNibName: NSNib.Name? { "ClipboardConfirmation" }
-
+/// This initializes a clipboard confirmation warning alert.
+class ClipboardConfirmationController: TextViewAlert {
     private(set) var confirmation: Ghostty.ClipboardConfirmationRequest
-    weak private var delegate: ClipboardConfirmationViewDelegate?
 
     init(
         confirmation: Ghostty.ClipboardConfirmationRequest,
-        delegate: ClipboardConfirmationViewDelegate
     ) {
         self.confirmation = confirmation
-        self.delegate = delegate
-        super.init(window: nil)
+        super.init()
+        let request = confirmation.kind
+        messageText = request.alertMessageText
+        informativeText = request.text()
+        alertStyle = request.alertStyle
+        textView.string = confirmation.contents
+        helpLink = request.alertHelpLink
+        addButton(withTitle: Action.confirm.text(request))
+        addButton(withTitle: Action.cancel.text(request))
+            .keyEquivalent = .init([KeyboardShortcut(.escape).key.character])
     }
 
     required init?(coder: NSCoder) {
@@ -31,30 +33,64 @@ class ClipboardConfirmationController: NSWindowController {
         guard self.confirmation !== confirmation else { return }
         self.confirmation = confirmation
 
-        guard isWindowLoaded, let window else { return }
-        configure(window)
-    }
+        let request = confirmation.kind
+        messageText = request.alertMessageText
+        informativeText = request.text()
+        alertStyle = request.alertStyle
+        textView.string = confirmation.contents
+        helpLink = request.alertHelpLink
 
-    // MARK: - NSWindowController
-
-    override func windowDidLoad() {
-        guard let window = window else { return }
-
-        configure(window)
-    }
-
-    private func configure(_ window: NSWindow) {
-        switch confirmation.kind {
-        case .paste:
-            window.title = "Warning: Potentially Unsafe Paste"
-        case .osc_52_read, .osc_52_write:
-            window.title = "Authorize Clipboard Access"
+        if let button = buttons[safe: 0] {
+            button.title = Action.confirm.text(request)
         }
 
-        window.contentView = NSHostingView(rootView: ClipboardConfirmationView(
-            contents: confirmation.contents,
-            request: confirmation.kind,
-            delegate: delegate
-        ))
+        if let button = buttons[safe: 1] {
+            button.title = Action.cancel.text(request)
+        }
+    }
+}
+
+private extension Ghostty.ClipboardRequest {
+    var alertMessageText: String {
+        let request = self
+        switch request {
+        case .paste:
+            return "Warning: Potentially Unsafe Paste"
+        case .osc_52_read, .osc_52_write:
+            return "Authorize Clipboard Access"
+        }
+    }
+
+    var alertHelpLink: String {
+        switch self {
+        case .paste:
+            "https://ghostty.org/docs/config/reference#clipboard-paste-protection"
+        case .osc_52_read, .osc_52_write:
+            "https://ghostty.org/docs/config/reference#clipboard-write"
+        }
+    }
+
+    var alertStyle: NSAlert.Style {
+        .informational
+    }
+}
+
+extension ClipboardConfirmationController {
+    enum Action: String {
+        case cancel
+        case confirm
+
+        func text(_ reason: Ghostty.ClipboardRequest) -> String {
+            switch (self, reason) {
+            case (.cancel, .paste):
+                return "Cancel"
+            case (.cancel, .osc_52_read), (.cancel, .osc_52_write):
+                return "Deny"
+            case (.confirm, .paste):
+                return "Paste"
+            case (.confirm, .osc_52_read), (.confirm, .osc_52_write):
+                return "Allow"
+            }
+        }
     }
 }
